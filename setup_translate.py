604b7442172bb6ca8d2e672a34bf1d759dada19a
Zdzislaw <zdzislaw22@windowslive.com>
2023-08-15 08:17:42 +0200

    avoid potential GS due to "TypeError: '>' not supported between instances of 'NoneType' and 'int'"

--- setup_translate.py
+++ setup_translate.py
@@ -6,36 +6,36 @@ import os
 
 
 class build_trans(cmd.Command):
-	description = 'Compile .po files into .mo files'
+    description = 'Compile .po files into .mo files'
 
-	def initialize_options(self):
-		pass
+    def initialize_options(self):
+        pass
 
-	def finalize_options(self):
-		pass
+    def finalize_options(self):
+        pass
 
-	def run(self):
-		s = os.path.join('IPTVPlayer', 'locale')
-		for lang in os.listdir(s):
-			lc = os.path.join(s, lang, 'LC_MESSAGES')
-			if os.path.isdir(lc):
-				for f in os.listdir(lc):
-					if f.endswith('.po'):
-						src = os.path.join(lc, f)
-						dest = os.path.join(lc, f[:-2] + 'mo')
-						print "Language compile %s -> %s" % (src, dest)
-						if os.system("msgfmt '%s' -o '%s'" % (src, dest)) != 0:
-							raise Exception, "Failed to compile: " + src
+    def run(self):
+        s = os.path.join('IPTVPlayer', 'locale')
+        for lang in os.listdir(s):
+            lc = os.path.join(s, lang, 'LC_MESSAGES')
+            if os.path.isdir(lc):
+                for f in os.listdir(lc):
+                    if f.endswith('.po'):
+                        src = os.path.join(lc, f)
+                        dest = os.path.join(lc, f[:-2] + 'mo')
+                        print ("Language compile %s -> %s" % (src, dest))
+                        if os.system("msgfmt '%s' -o '%s'" % (src, dest)) != 0:
+                            raise (Exception, "Failed to compile: " + src)
 
 
 class build(_build):
-	sub_commands = _build.sub_commands + [('build_trans', None)]
+    sub_commands = _build.sub_commands + [('build_trans', None)]
 
-	def run(self):
-		_build.run(self)
+    def run(self):
+        _build.run(self)
 
 
 cmdclass = {
-	'build': build,
-	'build_trans': build_trans,
+    'build': build,
+    'build_trans': build_trans,
 }
