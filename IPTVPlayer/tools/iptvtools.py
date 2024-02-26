f08c565accf6b52930baaa116274c15a49242a5f
Zdzislaw <zdzislaw22@windowslive.com>
2024-02-26 09:22:40 +0100

    pierdoła do przyszłego wykorzystania

--- IPTVPlayer/tools/iptvtools.py
+++ IPTVPlayer/tools/iptvtools.py
@@ -1889,6 +1889,13 @@ def isOPKGinstall():
     else:
         return False
 
+def hasCDM():
+    try:
+        from pywidevinecdm.checkCDMvalidity import testDevice
+        return testDevice()
+    except Exception:
+        return False
+
 def getIPTVplayerOPKGVersion():
     global CACHED_DATA_DICT
     if None == CACHED_DATA_DICT.get('IPTVplayerOPKGVersion', None):
